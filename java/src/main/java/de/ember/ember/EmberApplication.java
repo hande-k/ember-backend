289a82dd92bb6b239fbfb4d09b41e1789b5967ed
FearlessTobi <thm.frey@gmail.com>
2025-06-14 14:04:48 +0200

    java: Add Spring Boot boilerplate

---
+++ java/src/main/java/de/ember/ember/EmberApplication.java
@@ -0,0 +1,13 @@
+package de.ember.ember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmberApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/de/ember/ember/EmberApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ember.ember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
